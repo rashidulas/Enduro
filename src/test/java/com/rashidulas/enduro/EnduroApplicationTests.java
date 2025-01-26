6edc9153bae704c5414c9c86e512fb2d01cd0a18
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2025-01-26 07:44:50 -0600

    Initial Setup

---
+++ src/test/java/com/rashidulas/enduro/EnduroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rashidulas.enduro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnduroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

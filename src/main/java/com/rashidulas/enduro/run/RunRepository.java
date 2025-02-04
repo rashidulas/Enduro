ac61ede1bebf7c6d940c85157c7f406df82e282c
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2025-02-03 22:30:18 -0600

    build rest api

--- src/main/java/com/rashidulas/enduro/run/RunRepository.java
+++ src/main/java/com/rashidulas/enduro/run/RunRepository.java
@@ -1,4 +1,32 @@
 package com.rashidulas.enduro.run;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
 public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    };
+
+    Optional<Run> findById(Integer id) {
+        return runs.stream()
+                .filter(run -> rin.id() == id )
+                .findFirst()
+    }
+
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    void update(Run run) {
+
+    }
 }
